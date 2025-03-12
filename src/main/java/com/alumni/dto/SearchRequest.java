c717b322259bc90c1467dd2d1a71fa2a4abbd99c
Chandane2402 <chandanrace0204@gmail.com>
2025-03-12 20:03:02 +0530

    Update SearchRequest.java

--- src/main/java/com/alumni/dto/SearchRequest.java
+++ src/main/java/com/alumni/dto/SearchRequest.java
@@ -1,5 +1,20 @@
 package com.alumni.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SearchRequest {
 
+    @NotBlank(message = "University name is required")
+    private String university;
+
+    @NotBlank(message = "Designation is required")
+    private String designation;
+
+    @Min(value = 1900, message = "Passout year must be a valid year")
+    private Integer passoutYear;
 }
